d1f9f0b3b547d3e24484b5e522c29a9d3fd66195
Floris <info@styxit.com>
2018-07-06 11:27:45 +0200

    Implement array acces in ApiResourceSet

--- src/Structures/ApiResourceSet.php
+++ src/Structures/ApiResourceSet.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Exonet\Api\Structures;
 
+use ArrayAccess;
 use ArrayIterator;
+use Exonet\Api\Exceptions\ValidationException;
 use IteratorAggregate;
 
 /**
  * An ApiResourceSet is a collection of several ApiResource instances that are retrieved from the API.
  */
-class ApiResourceSet implements IteratorAggregate
+class ApiResourceSet implements IteratorAggregate, ArrayAccess
 {
     /**
      * @var \Exonet\Api\Structures\ApiResource[] The returned resources.
@@ -52,5 +54,56 @@ class ApiResourceSet implements IteratorAggregate
     public function getIterator() : ArrayIterator
     {
         return new ArrayIterator($this->resources ?? []);
+    }
+
+    /**
+     * Whether an offset exists
+     *
+     * @param mixed $offset An offset to check for.
+     *
+     * @return boolean true on success or false on failure.
+     */
+    public function offsetExists($offset) : bool
+    {
+        return isset($this->resources[$offset]);
+    }
+
+    /**
+     * Offset to retrieve.
+     *
+     * @param mixed $offset The offset to retrieve.
+     *
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        return $this->resources[$offset];
+    }
+
+    /**
+     * Offset to set.
+     *
+     * @param mixed $offset The offset to assign the value to.
+     * @param mixed $value  The value to set.
+     *
+     * @throws ValidationException if the provided $value is not an ApiResource.
+     */
+    public function offsetSet($offset, $value) : void
+    {
+        if (!$value instanceof ApiResource) {
+            throw new ValidationException('Only ApiResources can be set.');
+        }
+
+        $this->resources[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @param mixed $offset The offset to unset.
+     */
+    public function offsetUnset($offset) : void
+    {
+        unset($this->resources[$offset]);
     }
 }
