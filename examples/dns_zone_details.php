fa2e7062b2c94994d69890e2f477518e957812d6
Styxit <info@styxit.com>
2019-04-26 16:28:59 +0200

    Create examples to get DNS details

---
+++ examples/dns_zone_details.php
@@ -0,0 +1,40 @@
+<?php
+
+// Run this script using: php examples/ticket_details.php <YOUR-TOKEN>
+
+require __DIR__.'/../vendor/autoload.php';
+
+$authentication = new Exonet\Api\Auth\PersonalAccessToken($argv[1]);
+
+$exonetApi = new Exonet\Api\Client($authentication);
+
+/*
+ * Get a single dns_zone resource. Because depending on who is authorized, the dns_zone IDs change, all dns_zones are
+ * retrieved with a limit of 1. From this result, the first DNS zone is used. In a real world scenario you would
+ * call something like `$zone = $exonetApi->resource('dns_zones')->get('VX09kwR3KxNo');` to get a single DNS zone
+ * by it's ID.
+ */
+$zones = $exonetApi->resource('dns_zones')->size(1)->get();
+// Show this message when there are no zones.
+if (empty($zones)) {
+    echo 'There are no zones available.';
+    die();
+}
+$zone = $zones[0];
+
+// Output DNS zone details.
+echo sprintf("\nDNS zone:\t%s\n", $zone->name);
+
+// Get the records for this zone.
+$records = $zone->records()->get();
+// Show records.
+foreach ($records as $record) {
+    echo sprintf(
+        "%s  %s   %s   %s\n",
+        $record->type,
+        $record->fqdn,
+        $record->ttl,
+        $record->content,
+    );
+}
+echo "\n";
---
+++ examples/dns_zones.php
@@ -0,0 +1,27 @@
+<?php
+
+// Run this script using: php examples/dns_zones.php <YOUR-TOKEN>
+
+use Exonet\Api\Structures\ApiResourceSet;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$authentication = new Exonet\Api\Auth\PersonalAccessToken($argv[1]);
+
+$exonetApi = new Exonet\Api\Client($authentication);
+
+// Get all DNS zones, limited to 20.
+$zones = $exonetApi->resource('dns_zones')->size(20)->get();
+
+$description = 'DNS zones (max 20):';
+echo sprintf(
+    "\n%s\n%s\n",
+    $description,
+    str_repeat('-', strlen($description))
+);
+
+foreach ($zones as $zone) {
+    echo sprintf("%s - %d records\n", $zone->name, count($zone->records()->raw()));
+}
+
+echo "\n";
