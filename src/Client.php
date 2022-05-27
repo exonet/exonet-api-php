990254590bd506d558745854874062f1089ee70c
Robbin Janssen <r.janssen@exonet.nl>
2022-05-27 09:23:38 +0000

    Bump PSR

--- src/Client.php
+++ src/Client.php
@@ -163,14 +163,10 @@ class Client implements LoggerAwareInterface
      * Set the logger instance to use.
      *
      * @param LoggerInterface $logger The log instance to use.
-     *
-     * @return self The current Client instance.
      */
-    public function setLogger(LoggerInterface $logger): self
+    public function setLogger(LoggerInterface $logger)
     {
         $this->logger = $logger;
-
-        return $this;
     }
 
     /**
