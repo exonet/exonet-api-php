33962bbb165ee485487fe1c4d25eb240e975d86e
Robbin Janssen <r.janssen@exonet.nl>
2022-05-27 08:56:38 +0000

    Do not let Guzzle (7) throw execptions

--- src/Client.php
+++ src/Client.php
@@ -163,14 +163,10 @@ class Client implements LoggerAwareInterface
      * Set the logger instance to use.
      *
      * @param LoggerInterface $log The log instance to use.
-     *
-     * @return self The current Client instance.
      */
-    public function setLogger(LoggerInterface $log): self
+    public function setLogger(LoggerInterface $logger): void
     {
-        $this->logger = $log;
-
-        return $this;
+        $this->logger = $logger;
     }
 
     /**
