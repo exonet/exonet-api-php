9d72f5dc754e0457b7c31247dc9ce04e35a0673e
robbinjanssen <robbinjanssen@users.noreply.github.com>
2023-10-09 07:32:03 +0000

    Apply php-cs-fixer changes

--- src/Exceptions/AuthorizationException.php
+++ src/Exceptions/AuthorizationException.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Exonet\Api\Exceptions;
 
-class AuthorizationException extends ExonetApiException
-{
-}
+class AuthorizationException extends ExonetApiException {}
